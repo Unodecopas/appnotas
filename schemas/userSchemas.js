55ef051be7e06fabd95e0c8893362bbddb52337f
Jesus <gallardomoralesjesus@gmail.com>
2022-04-25 18:43:14 +0200

    feat: schemas - registro de usuario

---
+++ schemas/userSchemas.js
@@ -0,0 +1,35 @@
+const Joi = require("Joi");
+const { generateError } = require("../helpers");
+
+const registerSchema = Joi.object().keys({
+  username: Joi.string()
+    .required()
+    .min(4)
+    .error(
+      generateError(400, "Campo USUARIO debe tener al menos 4 caracteres")
+    ),
+  password: Joi.string()
+    .min(4)
+    .required()
+    .error(
+      generateError(400, "Campo CONTRASEÑA debe tener al menos 4 caracteres")
+    ),
+  email: Joi.string()
+    .email()
+    .required()
+    .error(generateError(400, "Debe introducir un email valido")),
+  name: Joi.string()
+    .required()
+    .min(4)
+    .error(generateError(400, "Campo NAME debe tener al menos 4 caracteres")),
+  lastname: Joi.string()
+    .required()
+    .min(4)
+    .error(
+      generateError(400, "Campo LASTNAME debe tener al menos 4 caracteres")
+    ),
+});
+
+module.exports = {
+  registerSchema,
+};
