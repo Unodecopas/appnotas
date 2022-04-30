bf7c9eb803012473805fde039db1a85251aaf1f5
Jesus <gallardomoralesjesus@gmail.com>
2022-04-30 08:26:30 +0200

    docs: swagger - endpoints

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const logger = require("npmlog");
 const morgan = require("morgan");
 const { register, login, logout } = require("./controllers/userControllers");
 const {
-    createCategory,
-    editCategory,
-    deleteCategory,
+  createCategory,
+  editCategory,
+  deleteCategory,
 } = require("./controllers/categoriesControllers");
 const { isAuth } = require("./middlewares/isAuth");
 const { categoryExists } = require("./middlewares/categoryExists");
@@ -17,9 +17,14 @@ const userExists = require("./middlewares/userExists");
 
 const { getNotes } = require("./controllers/notesControllers");
 
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerJsDocs = YAML.load("./api-docs.yaml");
+
 // middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 
 // routes
 app.post("/register", register);
@@ -32,22 +37,22 @@ app.delete("/category/:name", categoryExists, isAuth, deleteCategory);
 
 //errors 404
 app.use((req, res) => {
-    res.status(404).send({
-        status: "error",
-        message: "Not found",
-    });
+  res.status(404).send({
+    status: "error",
+    message: "Not found",
+  });
 });
 
 //middleware errors
 app.use((error, req, res, next) => {
-    res.status(error.httpStatus || 500).send({
-        status: "error",
-        message: error.message,
-    });
+  res.status(error.httpStatus || 500).send({
+    status: "error",
+    message: error.message,
+  });
 });
 
 // server listening
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    logger.info("SERVER", `http://localhost:${PORT}`);
+  logger.info("SERVER", `http://localhost:${PORT}`);
 });
