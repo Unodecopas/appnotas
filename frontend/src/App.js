6a1eb9703a529004bda40249ddd45f8f2a73a3e5
Jesus <gallardomoralesjesus@gmail.com>
2022-05-30 19:02:11 +0200

    routes - HOME

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import HomePage from "./pages/HomePage";
 function App() {
   return (
     <div className="App">
       <header className="App-header">Header</header>
+      <main>
+        <Routes>
+          <Route path="/*" element={<HomePage />} />
+        </Routes>
+      </main>
     </div>
   );
 }
