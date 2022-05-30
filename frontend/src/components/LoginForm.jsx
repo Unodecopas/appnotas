131643cd61da92d4667b307d18d35fd377c61982
Jesus <gallardomoralesjesus@gmail.com>
2022-05-30 19:29:32 +0200

    login form

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <section className="form-control">
+      <form>
+        <label htmlFor="username">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Login</button>
+      </form>
+      <Link to="/register">¿No estás registrado?¡Regístrate!</Link>
+    </section>
+  );
 };
 
 export default LoginForm;
