825bb66f4be574de6175548bd3459f3ec4ff55b1
Jesus <gallardomoralesjesus@gmail.com>
2022-05-30 19:25:16 +0200

    register form

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const RegisterForm = () => {
-  return <div>RegisterForm</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+
+  return (
+    <section className="form-control">
+      <form>
+        <label htmlFor="username">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label htmlFor="email">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor="name">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label htmlFor="lastname">
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            placeholder="lastname"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+          />
+        </label>
+        <button>Registro</button>
+      </form>
+      <Link to="/login">¿Ya registrado? Inicia sesión</Link>
+    </section>
+  );
 };
 
 export default RegisterForm;
