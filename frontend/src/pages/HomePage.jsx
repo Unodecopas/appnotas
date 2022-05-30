29b1a4f450ae2fa77d526d1a6a4d6ee43ab5e492
Jesus <gallardomoralesjesus@gmail.com>
2022-05-30 20:22:19 +0200

    homepage - ahora obtiene una lista con los titulos de las notas

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useUser } from "../context/userContext";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [notes, setNotes] = useState([]);
+  const [user] = useUser();
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        const res = await fetch(
+          `http://localhost:4000/users/${user.username}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: user.token,
+            },
+          }
+        );
+        if (res.ok) {
+          const data = await res.json();
+          console.log(data);
+          setNotes(data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getNotes();
+  }, [user]);
+  return (
+    <section>
+      {notes.map((note) => {
+        return <li key={note.id}>{note.title}</li>;
+      })}
+    </section>
+  );
 };
 
 export default HomePage;
