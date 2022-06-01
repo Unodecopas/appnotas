4f4891887b5f9a1db37846e0c59022317063ab62
anagnz07 <anagnz07@gmail.com>
2022-06-01 19:50:56 +0200

    feat: login inputs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,23 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import LoginForm from "./components/LoginForm";
+import LoginForm from "./components/LoginForm/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 import HomePage from "./pages/HomePage";
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/*" element={<HomePage />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<RegisterForm />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/*" element={<HomePage />} />
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/register" element={<RegisterForm />} />
+                </Routes>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
