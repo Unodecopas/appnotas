319e96bbd228be123af36bcbf99ac877aa1e7802
Jesus <gallardomoralesjesus@gmail.com>
2022-06-03 17:52:45 +0200

    style:estilos header y forms

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,38 +4,40 @@ import { useUser } from "../../context/userContext";
 import "./Header.css";
 
 const Header = () => {
-    const [user, , logout] = useUser();
-    const navigate = useNavigate();
+  const [user, , logout] = useUser();
+  const navigate = useNavigate();
 
-    const handleLogout = async () => {
-        await logout();
-        navigate("/");
-    };
+  const handleLogout = async () => {
+    await logout();
+    navigate("/");
+  };
 
-    const handleSingUp = async () => {
-        navigate("/register");
-    };
+  const handleSingUp = async () => {
+    navigate("/register");
+  };
 
-    return (
-        <header className="header-container">
-            <Link to="/">
-                <h1>HackNotes</h1>
-            </Link>
-            <div className="header-container__user-area">
-                {user ? (
-                    <>
-                        <p>{user.username}</p>
-                        <button onClick={handleLogout}>Logout</button>
-                    </>
-                ) : (
-                    <>
-                        <Link to="/login">Login</Link>
-                        <button onClick={handleSingUp}>Sign Up</button>
-                    </>
-                )}
-            </div>
-        </header>
-    );
+  return (
+    <header className="header-container">
+      <Link to="/">
+        <h1>HackNotes</h1>
+      </Link>
+      <div className="header-container__user-area">
+        {user ? (
+          <>
+            <p>{user.username}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <button className="register" onClick={handleSingUp}>
+              Sign Up
+            </button>
+          </>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -4,60 +4,60 @@ import { useUser } from "../../context/userContext";
 import "./LoginForm.css";
 
 const LoginForm = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [user, setUser] = useUser();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useUser();
 
-    if (user) return <Navigate to="/" />;
+  if (user) return <Navigate to="/" />;
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await fetch("http://localhost:4000/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ username, password }),
-            });
-            if (res.ok) {
-                const data = await res.json();
-                console.log(data);
-                setUser(data);
-            }
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:4000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+        setUser(data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-    return (
-        <section>
-            <form className="form-control" onSubmit={handleLogin}>
-                <label htmlFor="username">
-                    <input
-                        type="text"
-                        name="username"
-                        id="username"
-                        placeholder="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </label>
-                <label htmlFor="password">
-                    <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        placeholder="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </label>
-                <button>Login</button>
-            </form>
-            <Link to="/register">¿No estás registrado?¡Regístrate!</Link>
-        </section>
-    );
+  return (
+    <section className="form">
+      <form className="form-control" onSubmit={handleLogin}>
+        <label htmlFor="username">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Login</button>
+      </form>
+      <Link to="/register">¿No estás registrado? ¡Regístrate!</Link>
+    </section>
+  );
 };
 
 export default LoginForm;
